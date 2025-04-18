a92699aca8bc57f1eb3e991756725a8dfc2d6a9c
Jeff Ripke <jripke74@gmail.com>
2025-04-18 08:36:48 -0700

    add plant object

---
+++ script.js
@@ -0,0 +1,31 @@
+// Step 1
+// In this workshop, you'll build a plant nursery catalog while
+// practicing JavaScript's Map and Set objects.
+
+// To represent each plant in your catalog, you'll use objects,
+// each with three properties:
+
+// commonName, the common name of the plant
+// scientificName, the scientific name (or latin name) of the plant
+// cultivar, the variety of the plant
+// Start by creating an object with the three properties above for
+// the Spanish lavender (scientific name Lavandula stoechas) of a
+// cultivar called Ballerina and assign it to a variable named
+// ballerina.
+
+const ballerina = {
+  commonName: "Spanish lavender",
+  scientificName: "Lavandula stoechas",
+  cultivar: "Ballerina",
+};
+
+// Step 2
+// Now create another object, with the same three properties, to
+// represent another cultivar of spanish lavender called Pretty Polly,
+// and assign it to a variable named prettyPolly.
+
+const prettyPolly = {
+  commonName: "Spanish lavender",
+  scientificName: "Lavandula stoechas",
+  coltivar: "Pretty Polly",
+};
