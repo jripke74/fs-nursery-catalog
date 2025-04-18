27f21764e3a066bb273b67b5f9cfee37fd637253
Jeff Ripke <jripke74@gmail.com>
2025-04-18 09:25:05 -0700

    add catalog

--- script.js
+++ script.js
@@ -27,5 +27,33 @@ const ballerina = {
 const prettyPolly = {
   commonName: "Spanish lavender",
   scientificName: "Lavandula stoechas",
-  coltivar: "Pretty Polly",
+  cultivar: "Pretty Polly",
 };
+
+// Step 3
+// The remaining plant objects have been added for you. Now it's time 
+// to create your catalog.
+
+// Like regular JavaScript objects, Map objects contain a collection 
+// of key-value pairs. To create a Map object, you can use the Map 
+// constructor:
+
+// Example Code
+// const myMap = new Map();
+// Declare a catalog variable and assign it a Map object.
+
+// Step 4
+// Map objects remember the order in which the keys are inserted in the 
+// map. To set a key-value pair in your map object, you can use the set method. 
+// The following sets the key "name" to the value "Jerry" in the myMap map object.
+
+// Example Code
+// myMap.set("name", "Jerry");
+// Set the "ballerina" key to the value of your ballerina object in your catalog 
+// map. Then, log your catalog to the console.
+
+// Note that to see your map object properly you'll have to open the browser console.
+
+const catalog = new Map();
+catalog.set("ballerina", ballerina);
+console.log(catalog);
